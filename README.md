59366c0b51f021cd5b6d7d26e46176de4dd9a61d
b4rtik <andrea.ghelli@gmail.com>
2019-04-26 21:00:20 +0200

    First submite

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # SharpAdidnsdump
+
+SharpAdidnsdump is a c# implementation of Dirk-jan Mollema research: [Getting in the zone dumping active directory dns with adidnsdump](https://dirkjanm.io/getting-in-the-zone-dumping-active-directory-dns-with-adidnsdump/).
+
+All the credits go to Dirk-jan Mollema and his research.
+
+# Features
+
+Enumerate all hosts with IPs via LDAP and DNS query.
+
+The first step is to list the zones available in DomainDnsZone using the filter (&(objectClass = DnsZone)(!(DC = * harp))(!(DC = RootDNSServers))).
+
+For each zone it is possible to list all Host objects with the filter (&(!(ObjectClass = DnsZone))(!(DC = @))(!(DC = * harp))(!(DC = * DNSZones))) changing the RootDn of the query. It is necessary (! (ObjectClass = DnsZone)) because if the filter were used (objectClass = DnsNode) the hidden elements would be excluded.
+
+Some of the records present via LDAP can be listed as unlisted.
+In my implementation I resolve the visibility of these records with the parsoing of the Path property of the SearchResult object.
+
+### Usage
+
+SharpAdidnsdumpis.exe dc-address
+
+
+# References
+
+[Getting in the zone dumping active directory dns with adidnsdump](https://dirkjanm.io/getting-in-the-zone-dumping-active-directory-dns-with-adidnsdump/).
+
+[Adidnsdump](https://github.com/dirkjanm/adidnsdump)
+
+
+
+Feel free to contact me at: [@b4rtik](https://twitter.com/b4rtik)
+
+
---
+++ SharpAdidnsdump/Program.cs
@@ -0,0 +1,123 @@
+﻿
+// Author: B4rtik (@b4rtik)
+// Project: SharpAdidnsdump (https://github.com/b4rtik/SharpAdidnsdump)
+// License: BSD 3-Clause
+// based on 
+// Getting in the zone dumping active directory dns with adidnsdump
+// https://dirkjanm.io/getting-in-the-zone-dumping-active-directory-dns-with-adidnsdump/
+// by @_dirkjan
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.DirectoryServices;
+
+namespace SharpAdidnsdump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String dc_address = "";
+            if (args == null || args.Length <= 0)
+            {
+                Console.WriteLine("usage: SharpAdidnsdumpis.exe dc-address");
+                return;
+            }
+            else
+            {
+                dc_address = args[0];
+            }
+
+            try
+            {
+
+                Console.WriteLine("Running enumeration against {0}", dc_address);
+
+                String rootDn = "DC=DomainDnsZones";
+
+                String domain_local = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+
+                String domain_path = "";
+
+                foreach (String domain_path_r in domain_local.Split('.'))
+                {
+                    domain_path += ",DC=" + domain_path_r;
+                }
+
+                rootDn += domain_path;
+
+                Console.WriteLine("Running enumeration against {0}", "LDAP://" + dc_address + "/" + rootDn);
+
+                DirectoryEntry rootEntry = new DirectoryEntry("LDAP://" + dc_address + "/" + rootDn);
+                rootEntry.AuthenticationType = AuthenticationTypes.Delegation; //Or whatever it need be
+                DirectorySearcher searcher = new DirectorySearcher(rootEntry);
+
+                //find domain
+                var queryFormat = "(&(objectClass=DnsZone)(!(DC=*arpa))(!(DC=RootDNSServers)))";
+                searcher.Filter = queryFormat;
+                searcher.SearchScope = SearchScope.Subtree;
+
+                foreach (SearchResult result in searcher.FindAll())
+                {
+                    String domain = (result.Properties["DC"].Count > 0 ? result.Properties["DC"][0].ToString() : string.Empty);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("Domain: {0}", domain);
+                    Console.WriteLine();
+
+                    DirectoryEntry rootEntry_d = new DirectoryEntry("LDAP://" + dc_address + "/DC=" + result.Properties["DC"][0].ToString() + ",CN=microsoftdns," + rootDn);
+                    rootEntry_d.AuthenticationType = AuthenticationTypes.Delegation;
+                    DirectorySearcher searcher_h = new DirectorySearcher(rootEntry_d);
+
+                    //find hosts
+                    queryFormat = "(&(!(objectClass=DnsZone))(!(DC=@))(!(DC=*arpa))(!(DC=*DNSZones)))";
+                    searcher_h.Filter = queryFormat;
+                    searcher_h.SearchScope = SearchScope.Subtree;
+
+                    foreach (SearchResult result_h in searcher_h.FindAll())
+                    {
+                        String target = "";
+
+                        if (result_h.Properties["DC"].Count > 0)
+                        {
+                            target = result_h.Properties["DC"][0].ToString();
+                        }
+                        else
+                        {
+                            //Hidden entry
+                            String path = result_h.Path;
+                            target = (path.Substring(path.IndexOf("LDAP://" + dc_address + "/"), path.IndexOf(","))).Split('=')[1];
+                        }
+
+                        if (!target.EndsWith("."))
+                            target += "." + domain;
+
+                        try
+                        {
+                            IPHostEntry hostInfo = Dns.GetHostEntry(target);
+                            Console.WriteLine("Host {0} {1}", target, hostInfo.AddressList[0]);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("DNS Query with target : {0} failed", target);
+                        }
+
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("SharpAdidnsdump end");
+                Console.WriteLine();
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error retriving data : {0}", e.Message);
+                return;
+            }
+
+        }
+    }
+}
+
